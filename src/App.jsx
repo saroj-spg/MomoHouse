934c51425e696ecd9c7091f82dd262babf8194e1
saroj-spg <sarojpgautamgithub@gmail.com>
2025-01-31 16:26:58 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AllergyAdvice from './Pages/AllergyAdvice';
 import Contact from './Pages/Contact';
 import Login from './Pages/Login';
 import Menu from './Pages/Menu';
-import SignUp from './Pages/SignUp';
+import Signup from './Pages/Signup';
 import Momo from './Pages/Momo';
 import NavBar from './Components/NavBar';
 import ProductDescription from './Pages/ProductDescription';
@@ -29,7 +29,7 @@ const App = () => {
         <Route path="/advice" element={<AllergyAdvice />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/cart" element={<Protected Comp={Cart} />} />
         <Route path="/payment" element={<Protected Comp={Payment} />} />
         <Route path="/success" element={<Protected Comp={Success} />} />
